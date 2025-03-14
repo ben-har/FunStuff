9ac0a2a362523df0ff04b58582d2ea7c3cc25d8d
BenKevin harris <70765072+ben-har@users.noreply.github.com>
2025-03-14 15:03:36 -0500

    Add files via upload

---
+++ basiccalculator.cs
@@ -0,0 +1,73 @@
+﻿namespace Calctwo
+
+    // have it have a pref
+{
+    public class calculate
+    {
+        public int Calc()
+        {
+            
+            Console.Write("A value: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("B Value: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("multiplication|*|/|+|-|Sr|S|: ");
+            string multiplication = Console.ReadLine();
+
+            switch (multiplication)
+            {
+                case "*":
+                    return a * b;
+                case "/":
+                    
+                    if(a == 0 || b == 0 )
+                    {
+                        Console.WriteLine("syntax error");
+                        return 0000;
+                        
+                    }
+                    return a / b;
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "Sr":
+                    return a / a;
+                case "S":
+                    return a * a;
+                default:
+                    return 0000;
+
+
+
+
+
+
+
+            }
+
+
+            
+
+
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            calculate calc = new calculate();
+
+            int result = calc.Calc();
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
